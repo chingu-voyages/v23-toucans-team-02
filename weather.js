70454fe895c6778c9c4c4fbe1570fe65eeef9154
Tom Hudson <trhudson@gmail.com>
2020-09-13 21:00:54 -0400

    added functionality for imperial / metric

--- weather.js
+++ weather.js
@@ -1,31 +1,63 @@
-const apiKey = ''
+const apiKey = '01488c5d026e0674a47f8899573414fb'
 var weatherTemperature;
 var weatherCity;
 var weatherDescription;
 var weatherImage;
+var units;
 
 function checkWeather() {
+  setTemperatureChoice();
 
   var checkWeatherButton = document.getElementById('check-button')
+
   checkWeatherButton.onclick = function() {
     var checkWeatherInput = document.getElementById('check-input').value
+    getTemperatureChoice();
     getWeather(checkWeatherInput)
   }
 
   if (localStorage.getItem('weatherInput') !== null) {
     document.getElementById('check-input').value = localStorage.getItem('weatherInput');
     var checkWeatherInput = document.getElementById('check-input').value
+    getTemperatureChoice();
     getWeather(checkWeatherInput)
   }
 }
 
+function getTemperatureChoice() {
+  let measurementChoice = localStorage.getItem('tempChooser');
+
+  console.log("getTemp: " + measurementChoice);
+
+  // if (measurementChoice !== null) {
+    if (measurementChoice === "imperial") {
+      console.log("no");
+      document.getElementById('imperial').checked = true;
+      document.getElementById('metric').checked = false;
+    } else if (measurementChoice === "metric") {
+      console.log("yes");
+      document.getElementById('imperial').checked = false;
+      document.getElementById('metric').checked = true;
+    };
+
+  // }
+}
+
+function setTemperatureChoice() {
+  // refer to index.html to get metric or imperial
+  const measurementChoice = document.getElementById("imperial").checked;
+
+  if (measurementChoice === true) {
+    units = "imperial";
+    localStorage.setItem('tempChooser', "imperial");
+  } else {
+    units = "metric";
+    localStorage.setItem('tempChooser', "metric");
+  };
+}
+
 function getWeather(checkWeatherInput) {
   const method = isNaN(checkWeatherInput) ? 'q' : 'zip'
-
-  // refer to index.html to get metric or imperial
-  let units = "";
-  units = "imperial";
-
   const query = `https://api.openweathermap.org/data/2.5/weather?${method}=${checkWeatherInput}&units=${units}&APPID=${apiKey}`
 
   localStorage.setItem('weatherInput', checkWeatherInput);
