b3eea5506452d8d9c9e2c17c9195760149dd2290
Tom Hudson <trhudson@gmail.com>
2020-09-22 17:13:15 -0400

    Share Weather
    1. Feature
      a. created share.js functions to read and create URL

--- js/share.js
+++ js/share.js
@@ -1,30 +1,25 @@
-// // TODO:
-// User Story Description
-// As an end user
-// I want to share my current weather conditions on a social media outlet of my choice
-// So I can let my friends & family know what the weather is where I'm at
-//
-// Steps to Follow (optional)
-//
-//  Design link to accept parameter for user current weather
-//  Post URL to social media - Facebook & Twitter
-//  Design API functions to post to Facebook & Twitter
-//  Interpret URL parameters when page is displayed to display weather for specific location
-// Additional Considerations
-//
-// tbd
-
-
-// I'm looking at the share on social media and I believe I will have to use Node.js.
-// When the user shares, it will share the city/zip that they entered and pass that
-// in the URL that is shared. When it is shared and a new person clicks the link it
-// will see the extra info in the url (city/zip) and then fun the weather.js functions
-// to pull the weather and populate index.html. While I'm in node, I could
-// setup the dotenv as well. Thoughts?
-
-
-
-function getUrlParams(){
-  const queryString = window.location.search;
+function readUrlParams() {
+  const queryString = document.URL;
   console.log("URL: " + queryString);
+
+  // IF STATEMENT - if it was shared then execute this function
+
+  var posLat = queryString.search("Lat");
+  var posLog = queryString.search("Log");
+
+  document.getElementById("check-input-lat").value = queryString.slice(posLat, posLog);
+  document.getElementById("check-input-log").value = queryString.slice(posLog, 6);
+
+
+}
+
+function sendUrlParams() {
+  var userLog = "Log" + document.getElementById("check-input-log").value;
+  var userLat = "Lat" + document.getElementById("check-input-lat").value;
+
+  // BUILD URL TO SHARE
+  const queryString = document.URL + userLat + userLog;
+  console.log(queryString);
+
+
 }
