b53c840394a1b376663fea7e7238f1c2272d314f
Tom Hudson <trhudson@gmail.com>
2020-09-22 17:31:49 -0400

    upated share.js

--- js/share.js
+++ js/share.js
@@ -1,14 +1,21 @@
+const baseUrl = "https://reverent-kilby-ece0be.netlify.app"
+
 function readUrlParams() {
   const queryString = document.URL;
   console.log("URL: " + queryString);
 
   // IF STATEMENT - if it was shared then execute this function
+  var baseUrlLength = baseUrl.length;
+  var currentUrl = queryString.length;
 
-  var posLat = queryString.search("Lat");
-  var posLog = queryString.search("Log");
+  if (baseUrlLength !== currentUrl) {
+    console.log("this is a shared URL");
+    var posLat = queryString.search("Lat");
+    var posLog = queryString.search("Log");
 
-  document.getElementById("check-input-lat").value = queryString.slice(posLat, posLog);
-  document.getElementById("check-input-log").value = queryString.slice(posLog, 6);
+    document.getElementById("check-input-lat").value = queryString.slice(posLat, posLog);
+    document.getElementById("check-input-log").value = queryString.slice(posLog, 6);
+  }
 
 
 }
@@ -18,7 +25,7 @@ function sendUrlParams() {
   var userLat = "Lat" + document.getElementById("check-input-lat").value;
 
   // BUILD URL TO SHARE
-  const queryString = document.URL + userLat + userLog;
+  const queryString = baseUrl + userLat + userLog;
   console.log(queryString);
 
 
